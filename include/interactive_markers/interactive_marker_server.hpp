5062dc3566f5641fcb527b227d18e2e62b69f75b
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-07-16 21:13:21 +0200

    Rebase from 'release/rolling/interactive_markers'

--- include/interactive_markers/interactive_marker_server.hpp
+++ include/interactive_markers/interactive_marker_server.hpp
@@ -46,6 +46,7 @@
 #include "visualization_msgs/msg/interactive_marker_update.hpp"
 #include "visualization_msgs/srv/get_interactive_markers.hpp"
 
+#include "interactive_markers/node_interfaces.hpp"
 #include "interactive_markers/visibility_control.hpp"
 
 namespace interactive_markers
@@ -59,8 +60,8 @@ namespace interactive_markers
 class InteractiveMarkerServer
 {
 public:
-  typedef visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr FeedbackConstSharedPtr;
-  typedef std::function<void (FeedbackConstSharedPtr)> FeedbackCallback;
+  using FeedbackConstSharedPtr = visualization_msgs::msg::InteractiveMarkerFeedback::ConstSharedPtr;
+  using FeedbackCallback = std::function<void (FeedbackConstSharedPtr)>;
 
   static const uint8_t DEFAULT_FEEDBACK_CB = 255;
 
@@ -87,7 +88,7 @@ public:
     const rclcpp::QoS & update_pub_qos = rclcpp::QoS(100),
     const rclcpp::QoS & feedback_sub_qos = rclcpp::QoS(1));
 
-  template<typename NodePtr>
+  template<ServerNodeInterfaces NodePtr>
   InteractiveMarkerServer(
     const std::string & topic_namespace,
     NodePtr node,
@@ -232,7 +233,7 @@ private:
     visualization_msgs::msg::InteractiveMarker int_marker;
   };
 
-  typedef std::unordered_map<std::string, MarkerContext> M_MarkerContext;
+  using M_MarkerContext = std::unordered_map<std::string, MarkerContext>;
 
   // represents an update to a single marker
   struct UpdateContext
@@ -248,7 +249,7 @@ private:
     std::unordered_map<uint8_t, FeedbackCallback> feedback_cbs;
   };
 
-  typedef std::unordered_map<std::string, UpdateContext> M_UpdateContext;
+  using M_UpdateContext = std::unordered_map<std::string, UpdateContext>;
 
   void getInteractiveMarkersCallback(
     const std::shared_ptr<rmw_request_id_t> request_header,
